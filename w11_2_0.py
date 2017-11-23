d3ad6d9390998d5fef85f6539632951c05610c35
frankieeder <33560003+frankieeder@users.noreply.github.com>
2017-11-23 14:34:48 -0800

    Add files via upload

---
+++ w11_2_0.py
@@ -0,0 +1,20 @@
+from fml_2_0 import *
+###########
+# WEEK 11 #
+###########
+
+WEEK_11_BOR_RAW = """Film (Distributor)	Weekend
+Gross	Total
+Gross	%
+Change	Week
+#
+1	Thor: Ragnarok (Disney)	$55.5 M	$211.5 M	-55%	2
+2	Murder on the Orient Express (Fox)	$25.5 M	$25.5 M	NEW	1
+3	Daddy's Home 2 (Paramount)	$23.5 M	$23.5 M	NEW	1
+4	A Bad Moms Christmas (STXfilms)	$10.3 M	$38.3 M	-39%	2
+5	Jigsaw (Lionsgate)	$2.9 M	$33.9 M	-56%	3
+6	Boo 2! A Madea Halloween (Lionsgate)	$2.1 M	$46.0 M	-54%	4
+7	Geostorm (Warner Bros.)	$1.7 M	$31.9 M	-47%	4
+8	Thank You for Your Service (Univ. / DWorks)	$1.6 M	$9.9 M	-28%	3
+9	Let There Be Light (Atlas Distribution)	$1.5 M	$6.3 M	-12%	3
+10	Happy Death Day (Universal)	$1.4 M	$55.1 M	-48%	5"""
