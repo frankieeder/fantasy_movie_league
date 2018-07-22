eb86645e10bb0c597faf7f46b0314662352a18f4
Frankie Eder <frankieeder129@gmail.com>
2018-07-22 14:15:42 -0700

    Succesfully split metascraping into separate files.

--- models/data_aggregation.py
+++ models/data_aggregation.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from models.utils import *
 import os
-import models.metascraping as ms
+import models.tmdb_searcher as tmdbs
 
 pd.set_option('display.max_rows', 50)
 pd.set_option('display.max_columns', 50)
@@ -19,65 +19,48 @@ BOM_table = pd.concat(BOM_tables)
 BOM_table.columns = replace_list(list(BOM_table.columns), {'Title (click to view)': "title", "LW": "LW Ranking"})
 BOM_good_cols = ['Weekend Gross', 'LW Ranking', 'title', 'Theater Count', 'Theater Change',
                  'Total Gross', 'Week #', 'year']
-BOM_numeric_cols = list(set(BOM_table.columns) - set("title"))
-BOM_table = BOM_table.loc[:, BOM_good_cols]
-BOM_table = BOM_table.replace(["-", "", "N"], value=np.NaN)
-BOM_table[BOM_numeric_cols] = BOM_table[BOM_numeric_cols].apply(lambda x: x.str.replace("[^0-9.-]", ""), 1).astype(float)
-apply_to_df(BOM_table, lambda x: parse_digit(x, float), columns=BOM_numeric_cols)
+BOM_categorical_cols = ["title"]
+BOM_numeric_cols = [c for c in BOM_good_cols if c not in BOM_categorical_cols]
+BOM_table = BOM_table[BOM_good_cols]
+BOM_table[BOM_numeric_cols] = BOM_table[BOM_numeric_cols].replace(regex=["[^0-9.-]", "^-$"], value=["", ""]).apply(pd.to_numeric, errors='coerce')
+BOM_table.loc[:, 'year'] = BOM_table['year'].astype(int) #TODO: This shouldn't be necessary tho!
 BOM_table['LW Total Gross'] = BOM_table['Total Gross'] - BOM_table['Weekend Gross']
 BOM_table.drop('Total Gross', 1)
-unique_BOM_movies = BOM_table.loc[:, ["title", "year"]].drop_duplicates()
-BOM_table.columns = replace_list(BOM_table.columns, {e: "bom_" + e for e in BOM_numeric_cols})
+unique_BOM_movies = BOM_table['title'].drop_duplicates()
+BOM_table.columns = ["bom_" + e for e in BOM_table.columns]
 
 
-#### IMDb
-IMDb_path = "../data/IMDb/output"
-IMDb_dirs = [os.path.join(cwd, IMDb_path, f) for f in os.listdir(IMDb_path) if f[-4:] == ".csv"]
-IMDb_tables = [pd.read_csv(f) for f in IMDb_dirs]
-IMDb_table = pd.concat(IMDb_tables)
-unique_IMDb_movies = IMDb_table.loc[:, ["title", "year"]].drop_duplicates()
-IMDb_table = IMDb_table.merge(unique_BOM_movies, on=["title", "year"])
-IMDb_table.columns = replace_list(IMDb_table.columns, {e: "imdb_" + e for e in ["year", "runtime", "genres", "directors", "cast", "numvotes", "metacritic_rating"]})
+#### TMDB
+# Pull crucial data, and merge to make initial df
+TMDB_ids = tmdbs.get_tmdb_movie_id.dict(unique_BOM_movies)
+TMDB_title_details = tmdbs.get_tmdb_movie_details.df(TMDB_ids.values())
+TMDB_title_crew = tmdbs.get_tmdb_movie_crew.df(TMDB_ids.values())
+TMDB_df = TMDB_title_details.merge(TMDB_title_crew, how='outer', on='id')
+# Cut useless columns
+TMDB_good_cols = ['id', 'belongs_to_collection', 'budget', 'genres',
+       'popularity', 'release_date', 'runtime',
+       'title', 'vote_average', 'cast', 'crew', 'production_companies']
+TMDB_df = TMDB_df[TMDB_good_cols]
+# Process release dates
+TMDB_year_splits = TMDB_df['release_date'].str.split("-", expand=True).apply(pd.to_numeric, errors='coerce')
+TMDB_df['release_year'] = TMDB_year_splits[0]
+TMDB_df['release_month'] = TMDB_year_splits[1]
+TMDB_df['release_day'] = TMDB_year_splits[2]
+TMDB_df = TMDB_df.drop('release_date', 1)
+# Process belongs_to_collection
+TMDB_df['belongs_to_collection'] = [(True if e else False) for e in TMDB_df['belongs_to_collection']]
+# Process genres
+genre_splitter = lambda x: [] if not x else [g['name'] for g in x]
+TMDB_genres = one_hot_encode(TMDB_df['id'], TMDB_df['genres'], genre_splitter)
+TMDB_df = TMDB_df.merge(TMDB_genres, left_on='id', right_index=True).drop('genres', 1)
 
-# Process and attach Actors, Directors, and Genres
-directors = IMDb_table.loc[:, "imdb_directors"].drop_duplicates()
-cast = IMDb_table.loc[:, "imdb_cast"].drop_duplicates()
-genres = IMDb_table.loc[:, "imdb_genres"].drop_duplicates()
-directordf = function_results_to_df(directors, ms.get_imdb_people_data, input_name="imdb_directors")
-castdf = function_results_to_df(cast, ms.get_imdb_people_data, input_name="imdb_cast")
-genresdf = function_results_to_df(genres, one_hot_encode, input_name="imdb_genres")
+# TODO: Process cast
+TMDB_df = TMDB_df.drop('cast', 1)
+# TODO: Process crew
+TMDB_df = TMDB_df.drop('crew', 1)
+# TODO: Process production_companies
+TMDB_df = TMDB_df.drop('production_companies', 1)
 
-meta_table = IMDb_table.merge(directordf).drop("imdb_directors", 1)
-meta_table = meta_table.merge(castdf).drop("imdb_cast", 1)
-meta_table = meta_table.merge(genresdf).drop("imdb_genres", 1)
+TMDB_df.columns = ["tmdb_" + e for e in TMDB_df.columns]
 
-modeling_data = BOM_table.merge(meta_table)
-good_cols = [
-    'bom_Weekend Gross', 'bom_LW Ranking', 'bom_LW Total Gross', 'bom_Theater Count', 'bom_Theater Change',
-     'bom_year', 'imdb_runtime', 'imdb_rating', 'imdb_metacritic_rating', 'imdb_numvotes',
-     # director vbls
-     'imdb_directors_num_wins', 'imdb_directors_Writer', 'imdb_directors_win_nom_ratio',
-     'imdb_directors_num_noms', 'imdb_directors_Director', 'imdb_directors_Producer',
-     'imdb_directors_Thanks', 'imdb_directors_Editor', 'imdb_directors_Self', 'imdb_directors_Special effects',
-     'imdb_directors_Art director', 'imdb_directors_star_rank', 'imdb_directors_Cinematographer',
-    'imdb_directors_Visual effects',
-     # cast vbls
-     'imdb_cast_Actor', 'imdb_cast_win_nom_ratio', 'imdb_cast_num_noms', 'imdb_cast_Self', 'imdb_cast_num_wins',
-     'imdb_cast_Actress', 'imdb_cast_star_rank', 'imdb_cast_Director', 'imdb_cast_Producer',
-     # genres
-     'imdb_genres_Biography', 'imdb_genres_Comedy', 'imdb_genres_Drama', 'imdb_genres_Thriller',
-     'imdb_genres_Action', 'imdb_genres_Crime', 'imdb_genres_Sport', 'imdb_genres_Family',
-     'imdb_genres_History', 'imdb_genres_Mystery', 'imdb_genres_Horror', 'imdb_genres_Western',
-     'imdb_genres_Adventure', 'imdb_genres_Romance', 'imdb_genres_Fantasy', 'imdb_genres_Animation',
-     'imdb_genres_Sci-Fi', 'imdb_genres_Musical', 'imdb_genres_War', 'imdb_genres_Music'
-]
-modeling_data = modeling_data.loc[:, good_cols]
-float_cols = ["imdb_rating", "imdb_directors_win_nom_ratio", "imdb_cast_win_nom_ratio"]
-int_cols = set(good_cols) - set(float_cols)
-na_cols = ["bom_LW Ranking", "bom_LW Total Gross", "bom_Theater Change"]
-apply_to_df(modeling_data, lambda x: 0 if pd.isnull(x) else x)
-apply_to_df(modeling_data, lambda x: np.NaN if x == 0 else x, columns=na_cols)
-apply_to_df(modeling_data, lambda x: parse_digit(x, caster=float), columns=float_cols)
-apply_to_df(modeling_data, lambda x: parse_digit(x, caster=int), columns=int_cols)
-modeling_data.to_csv("./modeling_data.csv")
 
